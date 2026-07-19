553f348d85020a4641780d2c235318188c2ba184
Munna <rahamathasanmunna100@gmail.com>
2026-07-19 07:24:33 +0600

    Update service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,22 +2,26 @@ const CACHE_NAME = "munna-ai-v1";
 
 const urlsToCache = [
   "/",
+  "/index.html",
   "/style.css",
   "/script.js",
-  "/profile.jpg"
+  "/manifest.json",
+  "/profile.jpg",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
